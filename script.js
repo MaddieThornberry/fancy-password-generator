b68ee5cb4b616cd282d95bc82b7e2754a2417ef7
maddie <madeleinethornberry@gmail.com>
2022-07-24 12:34:54 -0400

    Added some Psudo

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(){
+
+  /* 1. Prompt password criteria
+     2. Criteria length 8-128
+     3.character types (upper, lower, numeric, symbol) */
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +19,7 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// 1. create password
+// 2. loop for variables
+// 3. take out unchecked
+// 4. final password fill in
